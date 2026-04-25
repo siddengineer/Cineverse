4278d50ed4c597c6d19aa7a8f60f01a7c7364ebb
isiddharth34 <siddharthp718@gmail.com>
2026-04-25 17:48:49 +0530

    force sqlite for render

--- cineverse/settings.py
+++ cineverse/settings.py
@@ -243,24 +243,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'cineverse.wsgi.application'
 
 
-if os.environ.get('RENDER'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_USER'),
-            'PASSWORD': os.environ.get('DB_PASSWORD'),
-            'HOST': os.environ.get('DB_HOST'),
-            'PORT': '5432',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 DATABASE_URL = os.environ.get('DATABASE_URL', '')
 if DATABASE_URL.startswith('postgresql://'):
